71ad277711356446003bc743fdb6d5edba34fcdd
atulmcoder <matul6827@gmail.com>
2025-10-03 17:45:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,12 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+  origin:[ process.env.FRONTEND_URL || "http://localhost:5173",
+          "https://sbsauto.ca/"
+          ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-type", "Authorization"]
+  
 }));
 
 // Routes
